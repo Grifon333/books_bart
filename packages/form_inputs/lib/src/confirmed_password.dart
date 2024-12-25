4c02cbcae7aac09dd5643c006cb227d818ce7875
KroGen <daniil.king333@gmail.com>
2024-12-25 08:48:02 +0200

    Added package Form Inputs.

---
+++ packages/form_inputs/lib/src/confirmed_password.dart
@@ -0,0 +1,22 @@
+import 'package:formz/formz.dart';
+
+enum ConfirmedPasswordValidationError {
+  invalid
+
+  /// other
+}
+
+class ConfirmedPassword
+    extends FormzInput<String, ConfirmedPasswordValidationError> {
+  final String password;
+
+  const ConfirmedPassword.pure(this.password) : super.pure('');
+
+  const ConfirmedPassword.dirty({required this.password, String value = ''})
+      : super.dirty(value);
+
+  @override
+  ConfirmedPasswordValidationError? validator(String? value) {
+    return password == value ? null : ConfirmedPasswordValidationError.invalid;
+  }
+}
